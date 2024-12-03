ee05178d761eee9efd8277d70799c8729f29b1cb
Saief <A.S.Bokor@outlook.com>
2024-12-03 18:07:15 +0000

    game writing up the game skeleton in the conole

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const Gameboard = {
-    gameboard: [];
-};
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const dropToken = (column, player) => {
+        const availableCells = board.filter((row) => row[column].getValue)
+    } 
+}
+
