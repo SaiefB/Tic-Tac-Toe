19e99bce0847467448372dc1685e53487e286823
Saief <A.S.Bokor@outlook.com>
2024-12-04 20:11:29 +0000

    added some comments explaining the code

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function Gameboard() {
+function Gameboard() { // Factory function to hold private functions
     const rows = 3;
     const columns = 3;
-    const board = [];
+    const board = []; // board initialized
 
+    // For loop to create the board. other loop make the rows and inner loop makes the arrays within the rows
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
@@ -10,20 +11,21 @@ function Gameboard() {
         }
     }
 
-    const getBoard = () => board;
+    const getBoard = () => board; // function to retrieve board
 
-    const dropToken = (column, player) => {
+    const placeToken = (column, player) => { // places a token in the selected field
         const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
 
-        if (!availableCells.length) return;
+        if (!availableCells.length) return; // if cells are all taken then return
     };
 
-    const printBoard = () => {
+    const printBoard = () => { // prints the board
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
         console.log(boardWithCellValues);
     };
 
-    return { getBoard, dropToken, printBoard }
+    return { getBoard, dropToken, printBoard } // exposes the functon in the factory function to the global scope
+
 }
 
 
