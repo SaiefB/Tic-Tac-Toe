70d3e4f8fb515edc6c4d55e21dca92d8285e45b3
Saief <A.S.Bokor@outlook.com>
2024-12-06 14:49:39 +0000

    Added another Factory Function called ScreenController() which contains functions such as updateScreen(), ClickhandlerBoard

--- script.js
+++ script.js
@@ -100,4 +100,46 @@ function ScreeController() {
     const game = GameController();
     const playerTurnDiv = document.querySelector('.turn');
     const boardDiv = document.querySelector('.board');
+
+    const updateScreen = () => {
+        boardDiv.textContent = ""; //clear the board
+
+        // get the newest version of the board and player turn
+        const board = game.getBoard();
+        const activePlayer = game.getActivePlayer();
+
+        playerTurnDiv.textContent = `${activePlayer.name}'s turn...` // display player's turn
+
+        // Render board squares
+        board.forEach(row => {
+            row.forEach((cell, index) => {
+                //Anything clickable should be a button!!
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                // Create a data attribute to identify the column
+                // This makes it easier to pass into our 'playRound' function
+                cellButton.dataset.column = index
+                cellButton.textContent = cell.getValue();
+                boardDiv.appendChild(cellButton);
+            })
+        })
+    }
+
+    // Add event listeners for the board
+    function clickHandlerBoard(e) {
+        const selectedColumn = e.target.dataset.column;
+        //MAke sure I've clicked a column and not the gaps in between
+        if (!selectedColumn) return;
+
+        game.playRound(selectedColumn);
+        updateScreen();
+    }
+    boardDiv.addEventListener("click", clickHandlerBoard);
+
+    //Initial render
+    updateScreen();
+
+    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
 }
+
+ScreeController();
