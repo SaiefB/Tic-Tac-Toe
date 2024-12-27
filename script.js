7db12be9992b7dc3b308c9201bdc1f7cf0f66079
Saief <A.S.Bokor@outlook.com>
2024-12-27 00:16:31 +0000

    updates on the Gameboard function

--- script.js
+++ script.js
@@ -13,17 +13,20 @@
 
     const getBoard = () => board; // function to retrieve board
 
-    const placeToken = (clickedCell, player) => {
-        if (clickedCell < 0) {
+    const placeToken = (inputCell, player) => { // cell validation
+        if (inputCell < 0 || inputCell > 3) { // if cell less than 0 or more than 3 then invalid cell
             console.log("Invalid Cell Selected!");
             return false;
         }
 
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < columns; i++) {
-                const cell = board[i][j];
+        for (let i = 0; i < rows; i++) { // iterates through all three rows
+            for (let j = 0; j < columns; j++) { // iterates through all three cells within each row
+                const cell = board[i][j]; // assigns each cell to cell variable
+                if (cell.getValue() === 0) {
+                    cell.addToken(player);
+                    return true;
+                };
             }
-            const cell = board[i]
         }
     };
  }
@@ -32,4 +35,11 @@
 
  function Cell() {
 
+    getValue() {
+        
+    };
+
+    return {
+        getValue
+    }
  }
