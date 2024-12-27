e0aea8f74e55e60b3921fcedf4ec82cc4259e193
Saief <A.S.Bokor@outlook.com>
2024-12-27 19:33:46 +0000

    some updates

--- script.js
+++ script.js
@@ -28,18 +28,83 @@
                 };
             }
         }
+
+        console.warn("Cell is full.")
+        return false;
     };
- }
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    }
+
+    return { getBoard, placeToken, printBoard }
+}
 
  Gameboard();
 
- function Cell() {
+function Cell() {
+    let value = 0;
 
-    getValue() {
-        
+    const addToken = (player) => {
+        if (value === 0) {
+            value = player;
+        };
     };
 
+    const getValue = () => value;
+
     return {
+        addToken,
         getValue
+    };
+};
+
+function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+ ) {
+    const board = Gameboard();
+
+    const players = [
+
+        {
+            name: playerOneName,
+            token: 1
+        },
+        {
+            name: playerTwoName,
+            token: 2
+        }
+    ];
+
+    let activePlayer = players[0]
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playRound = (column) => {
+        console.log(
+            `Placing ${getActivePlayer().name}'s token into cell ${cell}...`
+        );
+        board.placeToken(column, getActivePlayer().token);
+
+        switchPlayerTurn();
+        printNewRound();
     }
- }
+
+    printNewRound()
+
+    return {
+        playRound,
+        getActivePlayer,
+        getBoard: board.getBoard
+    };
+}
